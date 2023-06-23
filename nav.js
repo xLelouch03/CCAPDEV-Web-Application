729e33fb4699f6483b032af37a5d248355798a09
xLelouch03 <hans_rejano@dlsu.edu.ph>
2023-06-23 17:35:41 +0800

    edited main logout button

--- nav.js
+++ nav.js
@@ -2,39 +2,43 @@ $(document).ready(function() {
     $('#logout').click(function() {
         window.location.href = '../index.html';
     });
-        // Search functionality
-        $("#searchbar").on("input", function() {
-            var input = $(this).val().toLowerCase();
-            if (input.trim() === "") {
-                $("#searchResultsDropdown").hide();
-                return;
-            }
-            
-            var dropdown = $("#searchResultsDropdown");
-            dropdown.empty();
-            
-            $(".dropdown-menu .dropdown-item").each(function() {
-                var destinationName = $(this).text().toLowerCase();
-                if (destinationName.includes(input)) {
-                    var item = $("<a>").attr("href", $(this).attr("href")).addClass("dropdown-item").text($(this).text());
-                    dropdown.append(item);
-                }
-            });
-            
-            if (dropdown.children().length > 0) {
-                dropdown.show();
-            } else {
-                dropdown.hide();
-            }
-        });
     
-        // Hide dropdown when clicking outside
-        $(document).click(function(e) {
-            if (!$(e.target).closest(".search-container").length) {
-                $("#searchResultsDropdown").hide();
-            }
+    $("#searchbar").on("input", function() {
+        var input = $(this).val().toLowerCase();
+        if (input.trim() === "") {
+          $("#searchResultsDropdown").hide();
+          return;
+        }
+      
+        var dropdown = $("#searchResultsDropdown");
+        dropdown.empty();
+      
+        $(".dropdown-menu li a").each(function() {
+          var destinationName = $(this).text().toLowerCase();
+          if (destinationName.includes(input)) {
+            var item = $("<a>")
+              .attr("href", $(this).attr("href"))
+              .addClass("dropdown-item")
+              .text($(this).text());
+            dropdown.append(item);
+          }
         });
-    });
+      
+        if (dropdown.children().length > 0) {
+          dropdown.show();
+        } else {
+          dropdown.hide();
+        }
+      });
+      
+      // Hide dropdown when clicking outside
+      $(document).click(function(e) {
+        if (!$(e.target).closest(".search-container").length) {
+          $("#searchResultsDropdown").hide();
+        }
+      });
+            
+});
     
 const dropdownToggle = document.getElementById('profile-dropdown-toggle');
 const dropdownMenu = document.getElementById('profile-dropdown-menu');
