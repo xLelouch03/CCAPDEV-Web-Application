b16adf1f16937481c21b1235833675904f6c3737
xLelouch03 <hans_rejano@dlsu.edu.ph>
2023-07-23 19:40:45 +0800

    fixed helpful and unhelpful count

--- js/establishment-reply.js
+++ js/establishment-reply.js
@@ -6,90 +6,98 @@ function getCurrentDate() {
     return currentDate;
 }
 
-function addFeedback(feedbackData, reviewContainer) {
+function addFeedback(reviewContainer, feedbackData) {
+  const feedbackArray = Array.isArray(feedbackData) ? feedbackData : [feedbackData];
+  let feedbackPositioner = reviewContainer.querySelector(".feedback-positioner");
+
+  if (!feedbackPositioner) {
+    // Create and append the .feedback-positioner element if it doesn't exist
+    feedbackPositioner = document.createElement("div");
+    feedbackPositioner.classList.add("feedback-positioner");
+    reviewContainer.appendChild(feedbackPositioner);
+  }
+
+  for (const feedback of feedbackArray) {
     const feedbackContainer = document.createElement("div");
     feedbackContainer.classList.add("feedback-container");
-    
 
-    // profile container
-    const profileContainer = document.createElement("div");
-    profileContainer.classList.add("profile-container");
+      // profile container
+      const profileContainer = document.createElement("div");
+      profileContainer.classList.add("profile-container");
 
-    // profile picture img
-    const profilePictureImg = document.createElement("img");
-    profilePictureImg.classList.add("profile-picture");
-    profilePictureImg.src = feedbackData.profilePictureSrc;
-    profileContainer.appendChild(profilePictureImg);
+      // profile picture img
+      const profilePictureImg = document.createElement("img");
+      profilePictureImg.classList.add("profile-picture");
+      profilePictureImg.src = feedback.profilePictureSrc;
+      profileContainer.appendChild(profilePictureImg);
 
-    // post details container div
-    const postDetailsContainer = document.createElement("div");
-    postDetailsContainer.classList.add("post-details-container");
+      // post details container div
+      const postDetailsContainer = document.createElement("div");
+      postDetailsContainer.classList.add("post-details-container");
 
-    // profile name span
-    const profileNameSpan = document.createElement("span");
-    profileNameSpan.classList.add("profile-name");
-    profileNameSpan.textContent = feedbackData.profileName;
-    postDetailsContainer.appendChild(profileNameSpan);
+      // profile name span
+      const profileNameSpan = document.createElement("span");
+      profileNameSpan.classList.add("profile-name");
+      profileNameSpan.textContent = feedback.profileName;
+      postDetailsContainer.appendChild(profileNameSpan);
 
-    // post date span
-    const postDateSpan = document.createElement("span");
-    postDateSpan.classList.add("post-date");
-    postDateSpan.textContent = feedbackData.postDate;
-    postDetailsContainer.appendChild(postDateSpan);
+      // post date span
+      const postDateSpan = document.createElement("span");
+      postDateSpan.classList.add("post-date");
+      postDateSpan.textContent = feedback.postDate;
+      postDetailsContainer.appendChild(postDateSpan);
 
-    // Add postDetailsContainer to the profile container
-    profileContainer.appendChild(postDetailsContainer);
+      // Add postDetailsContainer to the profile container
+      profileContainer.appendChild(postDetailsContainer);
 
-    feedbackContainer.appendChild(profileContainer);
+      feedbackContainer.appendChild(profileContainer);
 
-    // feedback content span
-    const feedbackContentSpan = document.createElement("span");
-    feedbackContentSpan.classList.add("review-content");
-    feedbackContentSpan.textContent = feedbackData.feedbackContent;
+      // feedback content span
+      const feedbackContentSpan = document.createElement("span");
+      feedbackContentSpan.classList.add("review-content");
+      feedbackContentSpan.textContent = feedback.feedbackContent;
 
-    // feedback content span to the feedback container
-    feedbackContainer.appendChild(feedbackContentSpan);
+      // feedback content span to the feedback container
+      feedbackContainer.appendChild(feedbackContentSpan);
 
-    // Create edit button container div
-    const editButtonContainer = document.createElement("div");
-    editButtonContainer.classList.add("edit-button-container");
+      // Create edit button container div
+      const editButtonContainer = document.createElement("div");
+      editButtonContainer.classList.add("edit-button-container");
 
-    // Create edit button
-    const editButton = document.createElement("button");
-    editButton.classList.add("edit-button");
-    editButton.dataset.bsToggle = "modal";
-    editButton.dataset.bsTarget = "#editFeedbackModal";
+      // Create edit button
+      const editButton = document.createElement("button");
+      editButton.classList.add("edit-button");
+      editButton.dataset.bsToggle = "modal";
+      editButton.dataset.bsTarget = "#editFeedbackModal";
 
-    // Create edit button icon
-    const editIcon = document.createElement("i");
-    editIcon.classList.add("fa", "fa-pencil-square-o");
-    editIcon.setAttribute("aria-hidden", "true");
+      // Create edit button icon
+      const editIcon = document.createElement("i");
+      editIcon.classList.add("fa", "fa-pencil-square-o");
+      editIcon.setAttribute("aria-hidden", "true");
 
-    // Append icon to the button, and the button to the edit button container
-    editButton.appendChild(editIcon);
-    editButtonContainer.appendChild(editButton);
+      // Append icon to the button, and the button to the edit button container
+      editButton.appendChild(editIcon);
+      editButtonContainer.appendChild(editButton);
 
-    // Append edit button container to the feedback container
-    feedbackContainer.appendChild(editButtonContainer);
+      // Append edit button container to the feedback container
+      feedbackContainer.appendChild(editButtonContainer);
 
-    // feedback container to the feedback positioner
-    const feedbackPositioner = document.getElementsByClassName("feedback-positioner")[0];
-    feedbackPositioner.appendChild(feedbackContainer);
+      // feedback container to the feedback positioner
+      feedbackPositioner.appendChild(feedbackContainer);
+    }
+  }
 
-    const reviewsContainer = document.getElementsByClassName("reviews-cont")[0];
-    reviewsContainer.appendChild(reviewContainer);
 
-    reviewContainer.getElementsByClassName("feedback-positioner")[0].appendChild(feedbackContainer);
-}
 
-document.getElementById("feedbackForm").addEventListener("submit", function(event) {
+document.getElementById("feedbackModal").addEventListener("submit", function(event) {
   event.preventDefault();
   submitFeedback();
 });
 
-function submitFeedback() {
+function submitFeedback(buttonElement) {
+  const reviewId = buttonElement.getAttribute("data-review-id");
   const feedbackModal = document.getElementById("feedbackModal");
-  const profileName = document.querySelector(".profile-name").textContent;
+  const profileName = buttonElement.closest(".review-container").querySelector(".profile-name").textContent;
   const feedbackContent = document.querySelector("#reviewContent").value;
   const postDate = getCurrentDate();
 
@@ -100,7 +108,22 @@ function submitFeedback() {
     profilePictureSrc: 'your_image_source'  // photo here
   };
 
-  addFeedback(feedbackData);
-  // Reset the form fields (optional)
-  document.getElementById("feedbackForm").reset();
+  const targetReviewContainer = document.getElementById(reviewId);
+
+  if (targetReviewContainer) {
+    const feedbackPositioner = targetReviewContainer.querySelector(".feedback-positioner");
+    addFeedback(feedbackPositioner, feedbackData);
+  }
 }
+
+function clearFeedbackForm() {
+  const feedbackForm = document.getElementById("feedbackModal");
+  const inputFields = feedbackForm.querySelectorAll("input, textarea");
+
+  inputFields.forEach((input) => {
+    input.value = "";
+  });
+}
+
+// Add event listener to the feedback modal
+document.getElementById("feedbackModal").addEventListener("hidden.bs.modal", clearFeedbackForm);
